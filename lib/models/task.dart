bf50c93947e755b9e168e20e9c0c955ce8723323
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-24 13:09:53 +0300

    feat: add task attributes

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,5 +1,17 @@
+import '../enums/priority.dart';
+
 class Task {
-  String id;
+  final String id;
   String title;
-  
+  final Priority priority;
+  DateTime? dueDate ;
+  bool isCompleted;  
+
+   Task({
+    required this.id,
+    required this.title,
+    required this.priority,
+    this.dueDate,
+    this.isCompleted = false, 
+  });
 }
